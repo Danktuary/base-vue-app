cd371449fd127d65bf6e22a11fe7a5b17d232c7d
Sanctuary <sanc.pw@gmail.com>
2020-10-12 16:03:36 -0400

    Fix router file import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router.js'
+import router from './router/index.js'
 import './registerServiceWorker.js'
 
 createApp(App).use(router).mount('#app')
