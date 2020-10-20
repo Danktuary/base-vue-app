cd27a47e646de78f8af1ba4251f356d1ce5b90d2
Sanctuary <sanc.pw@gmail.com>
2020-10-20 18:15:11 -0400

    Update user management page with Bulma styles

--- src/views/examples/administrative/users/UserManagement.vue
+++ src/views/examples/administrative/users/UserManagement.vue
@@ -2,45 +2,68 @@
 	<div class="user-management">
 		<div class="user-management-tools">
 			<form class="user-create" @submit.prevent="addUser({ ...userForm })">
-				<div>
-					<label for="user-id">ID: </label>
-					<input id="user-id" v-model="userForm.id" type="number" disabled />
+				<div class="field">
+					<label class="label" for="user-id">ID: </label>
+					<div class="control">
+						<input id="user-id" v-model="userForm.id" class="input" type="number" disabled />
+					</div>
 				</div>
-				<div>
-					<label for="user-username">Username: </label>
-					<input id="user-username" v-model="userForm.username" type="text" />
+				<div class="field">
+					<label class="label" for="user-username">Username: </label>
+					<div class="control">
+						<input id="user-username" v-model="userForm.username" class="input" type="text" />
+					</div>
 				</div>
-				<div>
-					<label for="user-discriminator">Discriminator: </label>
-					<input id="user-discriminator" v-model="userForm.discriminator" type="number" min="0" max="9999" />
+				<div class="field">
+					<label class="label" for="user-discriminator">Discriminator: </label>
+					<div class="control">
+						<input id="user-discriminator" v-model="userForm.discriminator" class="input" type="number" min="0" />
+					</div>
 				</div>
-				<div>
-					<button type="submit">Submit</button>
+				<div class="field">
+					<button class="button is-primary" type="submit">Submit</button>
 				</div>
 			</form>
 			<br />
 			<form class="user-delete" @submit.prevent="deleteUser(userDeleteID)">
-				<div>
-					<label for="user-deletion-id">ID: </label>
-					<input id="user-deletion-id" v-model.number="userDeleteID" type="number" />
+				<div class="field">
+					<label class="label" for="user-deletion-id">ID: </label>
+					<div class="control">
+						<input id="user-deletion-id" v-model.number="userDeleteID" class="input" type="number" />
+					</div>
 				</div>
-				<div>
-					<button type="submit">Delete user</button>
+				<div class="field">
+					<div class="control">
+						<button class="button is-danger" type="submit">Delete user</button>
+					</div>
 				</div>
 			</form>
 			<br />
 			<form class="users-clear" @submit.prevent="clearUsers">
-				<div>
-					<button type="submit">Clear users</button>
+				<div class="field">
+					<div class="control">
+						<button class="button is-danger" type="submit">Clear users</button>
+					</div>
 				</div>
 			</form>
 		</div>
 		<div>
-			<ul>
-				<li v-for="user in users" :key="user.id">
-					#{{ user.id }}: {{ user.username }}#{{ user.discriminator }}
-				</li>
-			</ul>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Username</th>
+						<th>Discriminator</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="user in users" :key="user.id">
+						<td>{{ user.id }}</td>
+						<td>{{ user.username }}</td>
+						<td>{{ user.discriminator }}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </template>
@@ -76,10 +99,3 @@ export default {
 	},
 }
 </script>
-
-<style lang="sass">
-.user-management
-	.user-management-tools
-		.user-create
-			display: flex
-</style>
