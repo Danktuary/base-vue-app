66cb3b638f52a18768c325a011e1d6bb5e545b45
Sanctuary <sanc.pw@gmail.com>
2020-12-13 17:56:50 -0500

    feat(store): Add strict mode and dev logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'vuex'
+import { createLogger, createStore } from 'vuex'
 import user from './modules/user.js'
 import users from './modules/users.js'
 
 export default createStore({
+	strict: process.env.NODE_ENV !== 'production',
+	plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : [],
 	modules: {
 		user,
 		users,
